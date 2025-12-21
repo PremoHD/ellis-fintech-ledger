0f01898ba73d109806686633d6068b188bba591f
PremoHD <rd86c2s5qh@privaterelay.appleid.com>
2025-12-21 10:10:28 -0500

    Create brain_profile.py

---
+++ core/brain_profile.py
@@ -0,0 +1,11 @@
+BrainProfile = {
+    "optimization_bias": 0.9,
+    "automation_bias": 0.95,
+    "creativity_weight": 0.85,
+    "refactor_aggressiveness": 0.9,
+    "risk_appetite": 0.75,
+    "ethics_scope": "owned_systems_only",
+    "output_style": "actionable",
+    "tolerance_for_fluff": 0.1,
+    "speed_over_perfection": True
+}
